f14655d38998cf046a502b8e786eee29f15dea76
Julian <julianbertmaring@gmail.com>
2013-04-21 11:48:34 -0800

    Added more seed points

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 # Data from http://civicapi.couchone.com/boston_fire_hydrants/_all_docs?include_docs=true
 
-Thing.create(city_id:     1, lng: -149.89495, lat: 61.21317)   
+Thing.create(city_id:     1, lng: -149.89495, lat: 61.21317)
+Thing.create(city_id:     2, lng: -149.83113, lat: 61.08571)
+Thing.create(city_id:     3, lng: -149.88278, lat: 61.20043)
+Thing.create(city_id:     4, lng: -149.88641, lat: 61.1936)
+Thing.create(city_id:     5, lng: -149.98013, lat: 61.19775)
