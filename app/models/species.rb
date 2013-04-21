065b419ae6b3f19c39f5148308264e6740265ffb
mudlock <dale@sheldon-hess.org>
2013-04-20 18:20:32 -0800

    enh

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,5 +1,4 @@
 class Species < ActiveRecord::Base
   validates_uniqueness_of :species_id
-  validates_presence_of :common_name, :invasiveness_rank, :scientific_name, :species_code
-
+  validates_presence_of :code
 end
