9998ca23a73c7995bbcb21dbcf90dd0d35fa2bd7
Coral Sheldon-Hess <coral@sheldon-hess.org>
2013-04-20 21:20:57 -0800

    Revert "Update seeds.rb"
    This reverts commit c898181584c5f788b4d899792753d5c2d50a4986.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # Data from http://civicapi.couchone.com/boston_fire_hydrants/_all_docs?include_docs=true
 
-Thing.create(city_id:     1, lng: -149.89495, lat: 61.21317)
+Thing.create(city_id:     1, lng: -71.049226, lat: 42.383339)
 Thing.create(city_id:     2, lng: -71.075964, lat: 42.381021)
 Thing.create(city_id:     3, lng: -71.073419, lat: 42.380106)
 Thing.create(city_id:     4, lng: -71.070918, lat: 42.377728)
